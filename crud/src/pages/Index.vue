b027cd5dce4fa84e08fdfda563b400ea032d6fb4
wmacibnc <wmacibnc@gmail.com>
2020-01-26 12:18:49 -0300

    Enviando primeiro commit do projeto com quasar

---
+++ crud/src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list bordered separator>
+      <q-item clickable v-ripple>
+        <q-item-section>Single line item</q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Item with caption</q-item-label>
+          <q-item-label caption>Caption</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item v-for="(item, index) in list" v-bind:key="item" >
+        {{ item}} - {{ index}}
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label overline>OVERLINE</q-item-label>
+          <q-item-label>Item with caption</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Olá Mundo!',
+      list: ['Pessoas', 'Mundo', 'Ideias']
+    }
+  }
+}
+</script>
